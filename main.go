f3bdec84900f486956f91609d125d1c4d01dccba
Barnabas Kutassy <barnabas.kutassy@trivago.com>
2017-09-25 17:26:18 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) string {
+	dat, err := ioutil.ReadFile(fileName)
+	check(err)
+	return string(dat)
+}
+
+func createMap() map[string]string {
+	m := make(map[string]string)
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		m[pair[0]] = pair[1]
+	}
+	return m
+}
+
+func renderTemplateToFile(m map[string]string, text, outFileName string) {
+	f, err := os.Create(outFileName)
+	check(err)
+	t, err := template.New("templ").Parse(text)
+	check(err)
+	err = t.Execute(f, m)
+	f.Close()
+}
+
+func main() {
+	fileName := os.Args[1]
+	if !strings.HasSuffix(fileName, ".tpl") {
+		fmt.Println("No template file given: .tpl extension missing!")
+	}
+	text := readFile(fileName)
+	m := createMap()
+	renderTemplateToFile(m, text, strings.TrimSuffix(fileName, ".tpl"))
+}
